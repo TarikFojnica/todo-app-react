58f020a19d7efc3166416623dd641f4ee26546fc
TarikFojnica <tarik.f786@gmail.com>
2016-09-17 23:03:18 +0200

    Add the test Button component

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Button extends Component {
+	render() {
+		return (
+			<button>Test Button</button>
+		);
+	};
+}
+
+export default Button;
