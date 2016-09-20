c6f0a7c30104682215fbdf1dd91cc5031cb192d0
TarikFojnica <tarik.f786@gmail.com>
2016-09-20 14:54:49 +0200

    Iterate over elements with .map()

--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,20 +1,34 @@
 import React, {Component} from 'react';
-import request from 'superagent'
+import request from 'superagent';
+import _ from 'lodash'
 
 class WeatherDetails extends Component {
+	state = {
+		weatherData: [],
+	};
+
 	componentDidMount() {
+		var self = this;
 		request.get('https://api.forecast.io/forecast/d8ab77870812de67277ae47d3e9bf83e/43.8563,18.4131')
 			.end( function (err, res) {
-				console.log(res);
+				self.setState({
+					weatherData: res.body.daily.data
+				})
 			})
 	};
 
 	render() {
+		var names = ['Jake', 'Jon', 'Thruster'];
+		var namesList = this.state.weatherData.map((name) => {
+      <li>{name}</li>;
+      console.log(name)
+    });
+		
 		return (
-			<div className="element">
-
-			</div>
-		);
+			<div>
+				{namesList}
+	    </div>
+		)
 	};
 }
 
